efe2b5bd26feaf9ae649c34e18a40fc9067ca817
Andrew Van Slaars <avanslaars@users.noreply.github.com>
2017-10-26 20:17:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import './styles.css'
 
-export default class TodoApp extends Component {
+export default class App extends Component {
 
   render () {
     return <div>This is the app</div>
