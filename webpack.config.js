16ef9c2c21d6e7db7a70a759e5219cd09a232fe3
Andrew Van Slaars <avanslaars@gmail.com>
2018-09-16 18:41:52 -0400

    Upgraded webpack and associated plugins and loaders and converted from standard to eslint recommended

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,20 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 })
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'app.bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ }
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
     ]
   },
   devtool: 'sourcemap',
