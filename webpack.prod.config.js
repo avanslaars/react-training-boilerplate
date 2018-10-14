d767432bf1963af1bcf4bd1c0b60661f5bb59851
Andrew Van Slaars <avanslaars@gmail.com>
2018-10-14 09:37:08 -0400

    Added bundle analyzer for production builds

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,10 @@
 const path = require("path")
 const merge = require("webpack-merge")
 const base = require("./webpack.base.config")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
 
 module.exports = merge(base, {
-  mode: "production"
+  mode: "production",
+  plugins: [new BundleAnalyzerPlugin()]
 })
