96b1958397a1557974e9683561fa131a6ed72e58
Andrew Van Slaars <avanslaars@gmail.com>
2018-11-03 20:56:44 -0400

    Added jest-axe

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { render } from 'react-testing-library'
+import { axe } from 'jest-axe'
 import App from './App'
 
 describe('App', () => {
   it('Renders without blowing up', () => {
     const { getByText } = render(<App />)
     expect(getByText(/app/i)).toBeInTheDocument()
+  })
+
+  it('Does not have detectable accessibility issues', async () => {
+    const { container } = render(<App />)
+    expect(await axe(container.innerHTML)).toHaveNoViolations()
   })
 })
