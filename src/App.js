0950f3ee65be7d6cbca9abec6e0657947124145d
Olli <olli.e.oksanen@aalto.fi>
2019-10-30 13:48:34 +0200

    changed the header text to a fun essay

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is the app.</h1>
+        <h1>Once upon a time there was a HUGE GIANT MAMA bear. She lived in  a forest not too far away from the office here at futurice.
+          She was hungry and mad, now looking for food. A huge alarm was blaring and the noice was unbearable. And then the animal control came
+          and shot him. The end.
+        </h1>
       </div>
     )
   }
