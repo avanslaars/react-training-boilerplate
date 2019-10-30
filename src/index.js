02a690504ba6e76d72e7d8c3e7dfc5a71da8d7f5
Luka Salonen <luka.salonen@protonmail.com>
2019-10-30 13:40:40 +0200

    Add joke

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+// My grandfather has the heart of a lion and a lifetime ban at the zoo
